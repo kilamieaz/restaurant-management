46a5a7c4d1070b259ea895acb71253b2cbb6946d
kilamieaz <im.sultaan05@gmail.com>
2019-07-11 16:44:29 +0700

    laravel mix

--- webpack.mix.js
+++ webpack.mix.js
@@ -22,4 +22,11 @@ mix.js('resources/js/app.js', 'public/js')
     .scripts([
         'node_modules/moment/moment.js'
     ], 'public/js/moment.js')
+    //for nprogress.js
+    .scripts([
+        'node_modules/nprogress/nprogress.js'
+    ], 'public/js/nprogress.js')
+    .styles([
+        'node_modules/nprogress/nprogress.css'
+    ], 'public/css/nprogress.css')
     .sass('resources/sass/app.scss', 'public/css');
