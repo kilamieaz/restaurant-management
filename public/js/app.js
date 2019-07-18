9b7220485b4eb944012f0828e88d7bb92c97b88a
kilamieaz <im.sultaan05@gmail.com>
2019-07-18 11:31:27 +0700

    install datatable responsive

--- public/js/app.js
+++ public/js/app.js
@@ -74611,8 +74611,8 @@ if (token) {
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
-__webpack_require__(/*! C:\sultan\restaurant\resources\js\app.js */"./resources/js/app.js");
-module.exports = __webpack_require__(/*! C:\sultan\restaurant\resources\sass\app.scss */"./resources/sass/app.scss");
+__webpack_require__(/*! C:\sultan\restaurant-management\resources\js\app.js */"./resources/js/app.js");
+module.exports = __webpack_require__(/*! C:\sultan\restaurant-management\resources\sass\app.scss */"./resources/sass/app.scss");
 
 
 /***/ })
