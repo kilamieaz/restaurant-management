bc406a14b2ea3fd9886cda9122a599f82be92ab5
Sultan <im.sultaan05@gmail.com>
2019-07-18 14:26:45 +0700

    add middleware

--- routes/web.php
+++ routes/web.php
@@ -15,28 +15,31 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::prefix('admin')->group(function () {
-    // Route::resource('user', 'Admin\UserController');
-    Route::get('dashboard', 'Admin\DashboardController@index')->name('dashboard.index');
-    Route::resource('menu', 'Admin\MenuController');
-    Route::resource('category', 'Admin\CategoryController');
-    Route::resource('role', 'Admin\RoleController');
-    Route::resource('employee', 'Admin\EmployeeController');
-    Route::resource('member', 'Admin\MemberController');
-    Route::resource('table', 'Admin\TableController');
-    Route::resource('order', 'Admin\OrderController');
-    Route::resource('transaction', 'Admin\TransactionController');
+Route::group(['middleware' => ['auth']], function () {
+    Route::prefix('admin')->group( function () {
+        // Route::resource('user', 'Admin\UserController');
+        Route::get('dashboard', 'Admin\DashboardController@index')->name('dashboard.index');
+        Route::resource('menu', 'Admin\MenuController');
+        Route::resource('category', 'Admin\CategoryController');
+        Route::resource('role', 'Admin\RoleController');
+        Route::resource('employee', 'Admin\EmployeeController');
+        Route::resource('member', 'Admin\MemberController');
+        Route::resource('table', 'Admin\TableController');
+        Route::resource('order', 'Admin\OrderController');
+        Route::resource('transaction', 'Admin\TransactionController');
+    });
+
+    Route::prefix('datatable')->group(function () {
+        Route::get('menu-datatable', 'DataTable\MenuDataTableController@index')->name('datatable.menu');
+        Route::get('category-datatable', 'DataTable\CategoryDataTableController@index')->name('datatable.category');
+        Route::get('role-datatable', 'DataTable\RoleDataTableController@index')->name('datatable.role');
+        Route::get('employee-datatable', 'DataTable\EmployeeDataTableController@index')->name('datatable.employee');
+        Route::get('member-datatable', 'DataTable\MemberDataTableController@index')->name('datatable.member');
+        Route::get('table-datatable', 'DataTable\TableDataTableController@index')->name('datatable.table');
+        Route::get('order-datatable', 'DataTable\OrderDataTableController@index')->name('datatable.order');
+    });
 });
 
-Route::prefix('datatable')->group(function () {
-    Route::get('menu-datatable', 'DataTable\MenuDataTableController@index')->name('datatable.menu');
-    Route::get('category-datatable', 'DataTable\CategoryDataTableController@index')->name('datatable.category');
-    Route::get('role-datatable', 'DataTable\RoleDataTableController@index')->name('datatable.role');
-    Route::get('employee-datatable', 'DataTable\EmployeeDataTableController@index')->name('datatable.employee');
-    Route::get('member-datatable', 'DataTable\MemberDataTableController@index')->name('datatable.member');
-    Route::get('table-datatable', 'DataTable\TableDataTableController@index')->name('datatable.table');
-    Route::get('order-datatable', 'DataTable\OrderDataTableController@index')->name('datatable.order');
-});
 Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
