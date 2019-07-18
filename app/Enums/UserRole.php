e049d9c459f57b476f5136183a3962a0b7f59048
Sultan <im.sultaan05@gmail.com>
2019-07-18 14:25:12 +0700

    update enum

--- app/Enums/UserRole.php
+++ app/Enums/UserRole.php
@@ -7,8 +7,8 @@ use BenSampo\Enum\Enum;
 final class UserRole extends Enum
 {
     const Admin  = 0;
-    const Chef = 1;
-    const Waiter = 2;
-    const Cashier = 3;
+    const Cashier = 1;
+    const Chef = 2;
+    const Waiter = 3;
     const Member = 4;
 }
