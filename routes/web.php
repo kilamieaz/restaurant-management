6e08be558af5a3ec57652c94b7ee3aa389092ddf
Sultan <im.sultaan05@gmail.com>
2019-07-30 13:04:49 +0700

    redirect if authenticated

--- routes/web.php
+++ routes/web.php
@@ -53,4 +53,4 @@ Route::group(['middleware' => ['auth']], function () {
 
 Auth::routes();
 
-// Route::get('/home', 'HomeController@index')->name('home');
+Route::get('/home', 'HomeController@index')->name('home');
